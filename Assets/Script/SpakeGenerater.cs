bb03dc3c3440bd922f22c35ce8b27287e308f761
hmj26220318Osaki <hmj2622038@stu.o-hara.ac.jp>
2026-06-12 11:24:27 +0900

    弾を円状の螺旋状に攻撃するスクリプト

--- Assets/Script/SpakeGenerater.cs
+++ Assets/Script/SpakeGenerater.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SpakeGenerater : MonoBehaviour
 {
-    [SerializeField] private GameObject SpikedbollPrefab;   // 弾のプレハブ
-    [SerializeField] private int SpikedbollCount = 12;      // 発射する弾の数
+    [SerializeField] private GameObject attackPrefab;   // 弾のプレハブ
+    [SerializeField] private int attackCount = 12;      // 発射する弾の数
     [SerializeField] private float speed = 5f;              // 弾の移動速度
     [SerializeField] private float attackInterval = 1f;     // 攻撃のインターバル
+    [SerializeField] private float attackrotation = 15f;     // 攻撃の角度
+    private Rigidbody2D rigid;
     private float timer = 0f;
+    private float CurrentBaceRotation = 0f; // 現在の角度
 
     private void Start()
     {
-    
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,16 +22,29 @@ public class SpakeGenerater : MonoBehaviour
     {
         // タイマーを進める
         timer += Time.deltaTime;
-
-        // 3秒が経ったら攻撃
+        // 弾の発射間隔
         if (timer >= attackInterval)
         {
-            timer = 0f; // タイマーをリセット
+            AttackRotationCircle();
+            timer = 0f;
         }
+
+        // 現在の角度を回転させる
+        CurrentBaceRotation += Time.deltaTime * attackrotation;
+
+        // １周したら元に戻す
+        if (CurrentBaceRotation > 360f) CurrentBaceRotation -= 360f;
+
         // 画面外に出たらオブジェクトを破壊する
         if (transform.position.y < -5.0f)
         {
             Destroy(gameObject);
         }
+    }
+    private void AttackRotationCircle()
+    {
+        // 弾と弾の間の角度
+        float RotationStep = 360f / attackCount;
+
     }
 }
